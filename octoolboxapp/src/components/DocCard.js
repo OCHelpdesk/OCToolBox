b55aad81f7f032d2460d5bdef41d4ebc05a53203
Kevin Li <kandj.li.ca@gmail.com>
2023-12-30 11:21:10 -0500

    App: Before Trying RNFileViewer

--- octoolboxapp/src/components/DocCard.js
+++ octoolboxapp/src/components/DocCard.js
@@ -36,7 +36,6 @@ class DocCard extends Component {
             <View style={{flexDirection: 'row', height: 24}}>
               <Text style={styles.dataFieldCaption}>Category:</Text>
               <Text style={{flex: 1, }}>{this.props.doc.DocCategory}</Text>
-              <IconEx name='lock-open-outline' size={20} color='#999999' style={{width: 24, textAlign: "center", textAlignVertical: "top"}}/>
             </View>
             <View style={{flexDirection: 'row', height: 24}}>
               <Text style={styles.dataFieldCaption}>Name:</Text>
@@ -46,6 +45,7 @@ class DocCard extends Component {
             <View style={{flexDirection: 'row'}}>
               <Text style={styles.dataFieldCaption}>Description:</Text>
               <Text style={{flex: 1, }}>{this.props.doc.DocDescription}</Text>
+              <Icon name='ban' size={18} color='#ff6666' style={{width: 24, textAlign: "center", textAlignVertical: "top"}}/>
             </View>
           </TouchableOpacity>
         </View>
