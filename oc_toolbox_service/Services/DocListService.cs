2a65e2a30aefc2031cc842974e9a22d7a65a2168
orkincanada\song.li <sli@orkincanada.com>
2024-01-09 17:14:04 -0500

    DocType Changed to DocTypeEn and DocTypeFr

--- oc_toolbox_service/Services/DocListService.cs
+++ oc_toolbox_service/Services/DocListService.cs
@@ -63,10 +63,10 @@ namespace oc_toolbox_service.Services
                 SqlDataAdapter adpt = new SqlDataAdapter();
                 adpt.SelectCommand = new SqlCommand();
                 adpt.SelectCommand.Connection = conn;
-                adpt.SelectCommand.CommandText = "select DocType, DocNameEn, DocData=DocDataEn from Doc where DocId=" + docId;
+                adpt.SelectCommand.CommandText = "select DocType=DocTypeEn, DocNameEn, DocData=DocDataEn from Doc where DocId=" + docId;
                 if (inFrench != null && (bool)inFrench)
                 {
-                    adpt.SelectCommand.CommandText = "select DocType, DocNameEn, DocData=DocDataFr from Doc where DocId=" + docId;
+                    adpt.SelectCommand.CommandText = "select DocType=DocTypeFr, DocNameEn, DocData=DocDataFr from Doc where DocId=" + docId;
                 }
                 DataTable tbl = new DataTable();
                 adpt.Fill(tbl);
--- oc_toolbox_service/Services/DocListService.sql
+++ oc_toolbox_service/Services/DocListService.sql
@@ -13,34 +13,34 @@ BEGIN
 
 	select DocId, 
 		   Category = case when isnull(@inFrench, 0) = 0 then DocCategoryNameEn else DocCategoryNameFr end, 
-		   Type = DocType, 
-		   Name = case when isnull(@inFrench, 0) = 0 then DocNameEn else DocNameFr end, 
-		   Description = case when isnull(@inFrench, 0) = 0 then DocDescriptionEn else DocDescriptionFr end,
+		   Type = case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end, 
+		   Name = case when isnull(@inFrench, 0) = 0 then DocNameEn else isnull(DocNameFr, DocNameEn) end, 
+		   Description = case when isnull(@inFrench, 0) = 0 then DocDescriptionEn else isnull(DocDescriptionFr, DocDescriptionEn) end,
 		   DateLastUpdated = convert(nvarchar(10), LastUpdatedTime, 120), 
 		   SizeKB = cast((case when isnull(@inFrench, 0) = 0 then datalength(DocDataEn) else datalength(DocDataFr) end) / 8192 as int),
-		   FileName = replace(replace(replace(DocNameEn, '\', '_'), '/', '_'), '.', '_') + '.' + DocType,
+		   FileName = replace(replace(replace(DocNameEn, '\', '_'), '/', '_'), '.', '_') + '.' + (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end),
 		   IconName = 
 			case 
-			when DocType = 'pdf' then 'file-pdf-o'
-			when DocType in ('xlsx', 'xls') then 'file-excel-o'
-			when DocType in ('docx', 'doc') then 'file-word-o'
-			when DocType in ('pptx', 'ppt') then 'file-powerpoint-o'
-			when DocType in ('jpeg', 'jpg', 'png', 'bmp', 'heif', 'gif', 'tif', 'tiff') then 'file-image-o'
-			when DocType in ('mp3', 'wav', 'aiff') then 'file-audio-o'
-			when DocType in ('mp4', 'wov', 'wmv', 'avi') then 'file-video-o'
-			when DocType in ('txt') then 'file-text-o'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) = 'pdf' then 'file-pdf-o'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('xlsx', 'xls') then 'file-excel-o'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('docx', 'doc') then 'file-word-o'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('pptx', 'ppt') then 'file-powerpoint-o'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('jpeg', 'jpg', 'png', 'bmp', 'heif', 'gif', 'tif', 'tiff') then 'file-image-o'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('mp3', 'wav', 'aiff') then 'file-audio-o'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('mp4', 'wov', 'wmv', 'avi') then 'file-video-o'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('txt') then 'file-text-o'
 			else 'file-o'			
 			end,
 		   IconColor = 
 			case 
-			when DocType = 'pdf' then '#ff0000'
-			when DocType in ('xlsx', 'xls') then '#009900'
-			when DocType in ('docx', 'doc') then '#3366ff'
-			when DocType in ('pptx', 'ppt') then '#cc3300'
-			when DocType in ('jpeg', 'jpg', 'png', 'bmp', 'heif', 'gif', 'tif', 'tiff') then '#334938'
-			when DocType in ('mp3', 'wav', 'aiff') then '#474056'
-			when DocType in ('mp4', 'wov', 'wmv', 'avi') then '#589b8c'
-			when DocType in ('txt') then '#be4d4d'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) = 'pdf' then '#ff0000'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('xlsx', 'xls') then '#009900'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('docx', 'doc') then '#3366ff'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('pptx', 'ppt') then '#cc3300'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('jpeg', 'jpg', 'png', 'bmp', 'heif', 'gif', 'tif', 'tiff') then '#334938'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('mp3', 'wav', 'aiff') then '#474056'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('mp4', 'wov', 'wmv', 'avi') then '#589b8c'
+			when (case when isnull(@inFrench, 0) = 0 then DocTypeEn else DocTypeFr end) in ('txt') then '#be4d4d'
 			else '#990000'			
 			end,
 		   IsPublished
